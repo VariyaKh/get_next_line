3e8f229c16135c8317ec66eed08f9d10967acb7a
Variya Khuzhina <35562328+VariyaKh@users.noreply.github.com>
2019-01-04 20:21:20 +0300

    Add files via upload

---
+++ get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: drdraugr <variya1@yandex.ru>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/12/20 19:21:18 by drdraugr          #+#    #+#             */
+/*   Updated: 2019/01/04 17:21:24 by drdraugr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# define BUFF_SIZE 100
+
+int		get_next_line(const int fd, char **line);
+
+#endif
